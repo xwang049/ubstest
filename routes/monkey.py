427821b47d997ed9a562a42d81a7e84adbc6d049
xwang049 <xwang049@e.ntu.edu.sg>
2023-09-23 12:24:21 +0800

    upgrade 2 wxn

--- routes/monkey.py
+++ routes/monkey.py
@@ -7,7 +7,8 @@ from routes import app
 
 logger = logging.getLogger(__name__)
 from routes import GreedyMonkey
-
+from routes import lazy_developer1
+from routes import Railway
 @app.route('/greedymonkey', methods=['GET','POST'])
 def greedymonkey():
     data = request.get_json()
@@ -19,3 +20,23 @@ def greedymonkey():
 
 
 
+@app.route('/lazy-developer', methods=['GET','POST'])
+def hi():
+    data = request.get_json()
+    classes = data.get("classes")
+    statements = data.get("statements")
+    # return classes
+    return lazy_developer1.getNextProbableWords(classes=classes,
+                         statements=statements)
+
+    # return json.dumps(data)
+
+
+@app.route('/railway-builder', methods=['GET','POST'])
+def railwayBuilder():
+    data = request.get_json()
+    w = data.get("w")
+    v = data.get("v")
+    f = data.get("f")
+
+    return json.dumps(GreedyMonkey.greedyM(w,v,f))
