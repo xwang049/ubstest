b7e65859a3a5951b6d8cb3e12781bd82fb646dbe
qiyang14 <qiyangsun1@gmail.com>
2023-09-24 00:19:24 +0800

    maze

--- routes/railway.py
+++ routes/railway.py
@@ -2,42 +2,89 @@ import json
 import logging
 from flask import request
 from routes import app
-logger = logging.getLogger(__name__)
 
-def colony(inputdict):
-    ststr = inputdict['colony']
-    gen = inputdict['generations']
-    dp = [[0 for i in range(10)] for j in range(10)]
-    tmp = [[0 for i in range(10)] for j in range(10)]
-    sum = 0
-    for i in range(3):
-        dp[int(ststr[i])][int(ststr[i + 1])] += 1
-        sum += int(ststr[i])
-    sum += int(ststr[3])
-    for id in range(gen):
-        cur = sum % 10
-        for i in range(10):
-            for j in range(10):
-                nxt = (i - j + cur + 10) % 10
-                sum +=  nxt * dp[i][j]
-                tmp[i][nxt] += dp[i][j]
-                tmp[nxt][j] += dp[i][j]
-        for i in range(10): 
-            for j in range(10): 
-                dp[i][j] = tmp[i][j]
-                tmp[i][j] = 0
-    return sum
+flag = 1
+upd = -1
+input_data = {}
+ans_data = {}
 
-def work(inputlist):
-    ans = []
-    for x in inputlist:
-        ans.append(colony(x))
-    #print(ans)
-    return ans
+def output(str):
+    ans_data = {"playerAction": str}
+    flag = 0
 
-@app.route('/digital-colony', methods=['GET','POST'])
-def colonyBuilder():
-    data = request.get_json()
-    # decode_data = data.decode('utf-8')
-    # list1 = decode_data[1:-1].split(", ")
-    return json.dumps(work(data))
+def dfs(cur_pos, pt, dirs, ndirs, cur_map):
+    #input_dict = output()
+    pt[cur_pos] = cur_map
+    for i in range(4):
+        nxt_id = dirs[i][1]
+        nxt_val = cur_map[nxt_id[0]][nxt_id[1]]
+        dir = dirs[i][0]
+        nxt_pos = (dir[0] + cur_pos[0], dir[1] + cur_pos[1])
+        if nxt_val == 3 or nxt_val == 0: 
+            continue
+        elif nxt_pos in pt:
+            continue
+        input_dict = output(ndirs[i])
+        dfs(nxt_pos, pt, dirs, ndirs, input_dict["nearby"])
+        if i < 2:
+            output(ndirs[1 - i])
+        else:
+            output(ndirs[5 - i])
+    
+def findway(input_dict):
+    dirs = (((-1, 0), (1, 0)), ((1, 0), (1, 2)), ((0, 1), (0, 1)), ((0, -1), (2, 1)))
+    ndirs = ("left", "right", "up", "down")
+    pt = {}
+    dfs((0, 0), pt, dirs, ndirs, input_dict["nearby"])
+    m = {(0, 0): 0}
+    queue_list = [((0, 0), [])]
+    queue_pos = 0
+    queue_sz = 1
+    while queue_pos < queue_sz:
+        cur_pos = queue_list[queue_pos][0]
+        cur_str = queue_list[queue_pos][1]
+        cur_map = pt[cur_pos]
+        queue_pos += 1
+        for i in range(4):
+            nxt_id = dirs[i][1]
+            nxt_val = cur_map[nxt_id[0]][nxt_id[1]]
+            nxt_str = cur_str + [ndirs[i]]
+            if nxt_val == 3: 
+                ans = nxt_str
+                break
+            elif nxt_val == 0: 
+                continue
+            dir = dirs[i][0]
+            nxt_pos = (dir[0] + cur_pos[0], dir[1] + cur_pos[1])
+            if nxt_pos in m:
+                continue
+            m[nxt_pos] = m[cur_pos] + 1
+            queue_list.append((nxt_pos, nxt_str))
+            queue_sz += 1
+    
+    for x in ans:
+        output(x)
+            
+def work(input_data):
+    if flag == 1:
+        flag = -1
+        findway()
+    else:
+        
+    while upd == -1:
+        continue
+    return ans_data
+
+@app.route('/maze', methods=['GET','POST'])
+def mazeBuilder():
+    input_data = request.get_json()
+    # work(input_data)
+    # return work()
+    return json.dumps(work(input_data))
+
+
+# @app.route('/pie-chart', methods=['GET','POST'])
+# def mazeBuilder():
+#     input_data = request.get_json()
+#     # return work()
+#     return json.dumps(work())
