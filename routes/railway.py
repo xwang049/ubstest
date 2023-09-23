777760792618ab56932eb7b751984a7304a83f67
qiyang14 <qiyangsun1@gmail.com>
2023-09-23 17:52:57 +0800

    change colony

--- routes/railway.py
+++ routes/railway.py
@@ -1,92 +1,43 @@
-# import numpy as np
-
-# def RailwayCount(input_list):
-#     m = input_list[0]
-#     n = input_list[1]
-#     trlen = np.array(input_list[2:])
-#     dp = np.zeros(m + 1, dtype=int)
-#     dp[0] = 1
-#     for id in range(n):
-#         mul = 1
-#         tmp = np.zeros(m + 1, dtype=int)
-#         while 1:
-#             now = mul * trlen[id]
-#             if now > m: 
-#                 break
-#             for i in range(m - now + 1):
-#                 tmp[i + now] += dp[i]
-#             mul += 1
-#         for i in range(m + 1): 
-#             dp[i] += tmp[i]
-        
-#     return dp[m]
-
-# def Work(testlist):
-#     ans = []
-#     for str in testlist:
-#         str_array = str.split(',')
-#         for i in range(len(str_array)):
-#             str_array[i] = int(str_array[i])
-#         ans.append(RailwayCount(str_array))
-#     return ans
-
-# testlist = [
-#   "5, 3, 2, 1, 4",
-#   "3, 3, 4, 1, 2",
-#   "11, 1, 2"
-# ]
-# ans = Work(testlist)
-# print(ans)
 import json
 import logging
-
 from flask import request
-
 from routes import app
-
 logger = logging.getLogger(__name__)
 
-def RailwayCount(input_list):
-    m = input_list[0]
-    n = input_list[1]
-    trlen = input_list[2:]
-    dp = [0 for i in range(m + 1)] 
-    dp[0] = 1
-    for id in range(n):
-        mul = 1
-        tmp = [0 for i in range(m + 1)] 
-        while 1:
-            now = mul * trlen[id]
-            if now > m: 
-                break
-            for i in range(m - now + 1):
-                tmp[i + now] += dp[i]
-            mul += 1
-        for i in range(m + 1): 
-            dp[i] += tmp[i]
-        
-    return dp[m]
+def colony(inputdict):
+    ststr = inputdict['colony']
+    gen = inputdict['generations']
+    dp = [[0 for i in range(10)] for j in range(10)]
+    tmp = [[0 for i in range(10)] for j in range(10)]
+    sum = 0
+    for i in range(3):
+        dp[int(ststr[i])][int(ststr[i + 1])] += 1
+        sum += int(ststr[i])
+    sum += int(ststr[3])
+    for id in range(gen):
+        cur = sum % 10
+        for i in range(10):
+            for j in range(10):
+                nxt = (i - j + cur + 10) % 10
+                sum +=  nxt * dp[i][j]
+                tmp[i][nxt] += dp[i][j]
+                tmp[nxt][j] += dp[i][j]
+        for i in range(10): 
+            for j in range(10): 
+                dp[i][j] = tmp[i][j]
+                tmp[i][j] = 0
+    return sum
 
-def Work(testlist):
+def work(inputlist):
     ans = []
-    for str in testlist:
-        str_array = str.split(',')
-        for i in range(len(str_array)):
-            str_array[i] = int(str_array[i])
-        ans.append(RailwayCount(str_array))
+    for x in inputlist:
+        ans.append(colony(x))
+    #print(ans)
     return ans
 
-# testlist = [
-    
-# "5, 3, 2, 1, 4",
-# "3, 3, 4, 1, 2",
-# "11, 1, 2"
-# ]
-# ans = Work(testlist)
-# print(ans)
-
-
-@app.route('/railway-builder', methods=['GET','POST'])
-def railwayBuilder():
+@app.route('/digital-colony', methods=['GET','POST'])
+def colonyBuilder():
     data = request.get_json()
-    return json.dumps(Work(data))
+    # decode_data = data.decode('utf-8')
+    # list1 = decode_data[1:-1].split(", ")
+    return json.dumps(work(data))
